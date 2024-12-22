be3cdabea4100fc74ee505eae2dc1642aedbff45
Jenil Desai <jenildev91@gmail.com>
2024-12-23 01:10:54 +0530

    v2.1.1
    🧑🏻‍🔧 Fixed Carousel.

--- hooks/useCarousel.ts
+++ hooks/useCarousel.ts
@@ -1,7 +1,8 @@
 export function useCarousel(): { title: string; image: string }[] {
   return [
-    { title: "Wanderlust", image: "https://github.com/Jenil-Desai/Wander-lust/raw/main/screenshots/Mockup.png" },
-    { image: "https://github.com/Jenil-Desai/Zerodha-Clone/raw/main/Screenshot/mockup.png", title: "Zerodha Clone" },
-    { image: "https://github.com/Jenil-Desai/Verdant-Vow/raw/main/screenshot/Mockup.png", title: "Verdant Vow" },
+    { title: "GT3RS", image: "https://i.pinimg.com/736x/1b/9a/cc/1b9acc0fcaa4c5af0fd90b45ab3de286.jpg" },
+    { title: "Formula One", image: "https://plus.unsplash.com/premium_photo-1683134240084-ba074973f75e?q=80&w=3395&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { image: "https://plus.unsplash.com/premium_photo-1661952578770-79010299a9f9?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "Bhutan" },
+    { title: "Sikkim", image: "https://images.unsplash.com/photo-1674922049190-f631ba5bc44f?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
   ];
 }
