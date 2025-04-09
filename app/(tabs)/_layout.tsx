0cf865b269031130b213a0f64db8c77a48495578
Jenil Desai <jenildev91@gmail.com>
2025-04-09 16:51:21 +0530

    v3.5.1
    + Fixed White Bar On Top Of Application On Android.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,42 +1,37 @@
-import { Colors } from "@/constants/Colors";
+import {Colors} from "@/constants/Colors";
 import FontAwesome from "@expo/vector-icons/Ionicons";
 import {Tabs} from "expo-router";
-import { useColorScheme } from "react-native";
+import {useColorScheme} from "react-native";
 
 export default function TabLayout() {
-  const theme = useColorScheme() ?? "light";
+    const theme = useColorScheme() ?? "light";
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[theme].tint,
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: Colors[theme].background,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="foryou"
-        options={{
-          title: "For You",
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="person-outline" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Explore",
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="globe" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="account"
-        options={{
-          title: "Account",
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="settings-outline" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (<Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[theme].tint, headerShown: false, tabBarStyle: {
+                    backgroundColor: Colors[theme].background,
+                },
+            }}
+        >
+            <Tabs.Screen
+                name="foryou"
+                options={{
+                    title: "For You",
+                    tabBarIcon: ({color}) => <FontAwesome size={28} name="person-outline" color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Explore", tabBarIcon: ({color}) => <FontAwesome size={28} name="globe" color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="account"
+                options={{
+                    title: "Account",
+                    tabBarIcon: ({color}) => <FontAwesome size={28} name="settings-outline" color={color}/>,
+                }}
+            />
+        </Tabs>);
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,30 @@
-import {Stack} from "expo-router";
+import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import {AuthProvider} from "@/context/AuthContext";
+import { AuthProvider } from "@/context/AuthContext";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
+import { useColorScheme } from "react-native";
+import { Colors } from "@/constants/Colors";
 
 export default function Layout() {
+    const colorScheme = useColorScheme() ?? 'light';
+
     return (
-        <GestureHandlerRootView>
-            <AuthProvider>
-            <Stack screenOptions={{ headerShown: false }} initialRouteName="index">
-                <Stack.Screen name="index" options={{ headerShown: false, headerTitle: "Welcome" }} />
-                <Stack.Screen name="(auth)/login" options={{ headerShown: false, headerTitle: "Login", headerBackTitle: "Go Back" }} />
-                <Stack.Screen name="(nobottombar)/accountinfo" options={{ headerShown: true, headerTitle: "Account Info", headerBackTitle: "Go Back" }} />
-            </Stack>
-            </AuthProvider>
-        </GestureHandlerRootView>
+        <SafeAreaProvider>
+            <StatusBar
+                backgroundColor="transparent"
+                translucent={true}
+                barStyle={colorScheme === 'dark' ? 'light-content' : 'dark-content'}
+            />
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <AuthProvider>
+                    <Stack screenOptions={{ headerShown: false }} initialRouteName="index">
+                        <Stack.Screen name="index" options={{ headerShown: false, headerTitle: "Welcome" }} />
+                        <Stack.Screen name="(auth)/login" options={{ headerShown: false, headerTitle: "Login", headerBackTitle: "Go Back" }} />
+                        <Stack.Screen name="(nobottombar)/accountinfo" options={{ headerShown: true, headerTitle: "Account Info", headerBackTitle: "Go Back" }} />
+                    </Stack>
+                </AuthProvider>
+            </GestureHandlerRootView>
+        </SafeAreaProvider>
     );
 }
