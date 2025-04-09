32cdc55ef82a54321b4cbe0c73d1c4ce14109b70
Jenil Desai <jenildev91@gmail.com>
2025-04-09 22:27:43 +0530

    v3.6.1
    + Updated UI / UX For List Of Wallpapers.

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -1,51 +1,43 @@
-import { Wallpaper } from "@/hooks/fetchWallpapers";
-import {Image, StyleSheet, TouchableOpacity, View} from "react-native";
-import { ThemedText } from "./ThemedText";
-import { Ionicons } from "@expo/vector-icons";
+import {Wallpaper} from "@/hooks/fetchWallpapers";
+import {Image, Pressable, StyleSheet, TouchableOpacity} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
+import React from "react";
 
 interface ImageCardProps {
-  wallpaper: Wallpaper;
-  onPress: () => void;
-  onLikeToggle: () => void;
-  isLiked: boolean;
+    wallpaper: Wallpaper;
+    onPress: () => void;
+    onLikeToggle: () => void;
+    isLiked: boolean;
 }
 
-export default function ImageCard({ wallpaper, onPress,onLikeToggle, isLiked }: ImageCardProps) {
-  return (
-    <TouchableOpacity onPress={onPress}>
-        <Image source={{ uri: wallpaper.image_url }} style={style.image} />
-        <View style={style.labelContainer}>
-          <ThemedText style={style.label}>{wallpaper.title.slice(0, 10) + "..."}</ThemedText>
-          <TouchableOpacity style={style.iconContainer} onPress={onLikeToggle}>
-            <Ionicons name={"heart"} size={18} color={isLiked ? "pink" : "gray"} />
-          </TouchableOpacity>
-        </View>
-    </TouchableOpacity>
-  );
+export default function ImageCard({wallpaper, onPress, onLikeToggle, isLiked}: ImageCardProps) {
+    return (<TouchableOpacity onPress={onPress}>
+            <Image source={{uri: wallpaper.image_url}} style={style.image}/>
+            <Pressable style={style.labelContainer} onPress={onLikeToggle}>
+                <Ionicons name="heart" size={20} color={isLiked ? "pink" : "gray"} style={style.icon}/>
+            </Pressable>
+
+        </TouchableOpacity>);
 }
 
 const style = StyleSheet.create({
-  image: {
-    flex: 1,
-    height: 300,
-    borderRadius: 10,
-  },
-  labelContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: "rgba(0,0,0,0.5)",
-    position: "absolute",
-    bottom: 0,
-    width: "100%",
-    padding: 10,
-    borderBottomLeftRadius: 10,
-    borderBottomRightRadius: 10,
-  },
-  label: {
-    color: "white",
-  },
-  iconContainer: {
-    display: "flex",
-    justifyContent: "center",
-  },
+    image: {
+        flex: 1, height: 300, borderRadius: 10,
+    }, labelContainer: {
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        backgroundColor: "transparent",
+        position: "absolute",
+        bottom: 0,
+        width: "100%",
+        padding: 10,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+    }, label: {
+        color: "white",
+    }, iconContainer: {
+        display: "flex", justifyContent: "center",
+    }, icon: {
+        backgroundColor: "rgba(0,0,0,0.6)", padding: 5, borderRadius: 30,
+    },
 });
